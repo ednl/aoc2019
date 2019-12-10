7f21faa692927c40d42f338233fc517a1f81b96d
Ewoud Dronkert <ewoud@dronkert.net>
2019-12-10 14:26:21 +0100

    more permutations

--- perm.c
+++ perm.c
@@ -1,35 +1,65 @@
-#include <stdio.h>
+#include <stdio.h>   // printf
+#include <stdlib.h>  // malloc
 
-#define N 5
-#define M 10
-
-int main(void)
+void perm1(void)
 {
-	int a, b, c, d, e, i = 0;
+	int a, b, c, d, e;
 
-	for (a = N; a < M; ++a)
+	for (a = 0; a < 5; ++a)
 	{
-		for (b = N; b < M; ++b)
+		for (b = 0; b < 5; ++b)
 		{
 			if (b == a)
 				continue;
-			for (c = N; c < M; ++c)
+			for (c = 0; c < 5; ++c)
 			{
 				if (c == a || c == b)
 					continue;
-				for (d = N; d < M; ++d)
+				for (d = 0; d < 5; ++d)
 				{
 					if (d == a || d == b || d == c)
 						continue;
-					for (e = N; e < M; ++e)
+					for (e = 0; e < 5; ++e)
 					{
 						if (e == a || e == b || e == c || e == d)
 							continue;
-						printf("%3d: %d %d %d %d %d\n", ++i, a, b, c, d, e);
+						printf("%d%d%d%d%d\n", a, b, c, d, e);
 					}
 				}
 			}
 		}
 	}
+}
+
+void perm2(int n, int k)
+{
+	static int u[] = {0,0,0,0,0,0,0,0,0,0};  // index of used elements
+	static int r[] = {0,0,0,0,0,0,0,0,0,0};  // permutation result
+	int i;
+
+	for (i = 0; i < n; ++i)
+	{
+		if (u[i])             // is element i already in use?
+			continue;         // if so, skip it
+		r[k] = i;             // add unused element to the permutation
+		if (k < n - 1)        // more positions to go?
+		{
+			u[i] = 1;         // element i is now in use
+			perm2(n, k + 1);  // go to the next position
+			u[i] = 0;         // element i is no longer in use
+		} else                // this permutation is ready
+		{
+			// Do something with the permutation
+			// (but can't return a value from a recursive function)
+			for (i = 0; i < n; ++i)
+				printf("%d", r[i]);
+			printf("\n");
+		}
+	}
+}
+
+int main(void)
+{
+	perm2(3, 0);
 	return 0;
 }
